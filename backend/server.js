71f880cbaf075c7ec1a11facef1a8e17ffd9d7a3
Karin Nordkvist <kontakt@karinnordkvist.se>
2021-01-13 15:53:26 +0100

    First day. Signup, sign in and secret message done

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,123 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import mongoose from 'mongoose'
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import crypto from 'crypto';
+import bcrypt from 'bcrypt-nodejs';
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.Promise = Promise
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/authAPI';
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'You have to enter a name'],
+    unique: true,
+    minlength: 2,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString('hex'),
+    unique: true,
+  },
+});
+
+userSchema.pre('save', async function (next) {
+  const user = this;
+
+  // Check if the password is (not) modified
+  if (!user.isModified('password')) {
+    return next();
+  }
+
+  // If the password is changed- then encrypt it
+  const salt = bcrypt.genSaltSync();
+  console.log(`PRE- password before hash: ${user.password}`);
+  user.password = bcrypt.hashSync(user.password, salt);
+  console.log(`PRE- password after  hash: ${user.password}`);
+
+  // Continue with the save
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+const authenticateUser = async (req, res, next) => {
+  try {
+    const accessToken = req.header('Authorization');
+    const user = await User.findOne({ accessToken });
+    if (!user) {
+      throw 'User not found!';
+    }
+    req.user = user;
+    next();
+  } catch (err) {
+    res.status(401).json({ error: 'Please try again' });
+  }
+};
+
+// Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const port = process.env.PORT || 8080;
+const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+  res.send('Hello world');
+});
+
+// Signup
+app.post('/users', async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const user = new User({ name, email, password });
+    user.save();
+    res.status(200).json({ id: user._id, accessToken: user.accessToken });
+  } catch (error) {
+    res
+      .status(400)
+      .json({ message: 'Could not create user', errors: error.errors });
+    console.log({ error: err });
+  }
+});
+
+// Log in
+app.post('/sessions', async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (user && bcrypt.compareSync(req.body.password, user.password)) {
+    res.json({ id: user._id, accessToken: user.accessToken });
+  } else {
+    res.status(400).json({ message: 'Could not log in' });
+  }
+});
+
+// Private area - to access this endpoint you'll need to send a request with
+// a header with a value of a valid access token.
+app.get('/private', authenticateUser);
+app.get('/private', async (req, res) => {
+  const accessToken = req.header('Authorization');
+  const user = await User.findOne({ accessToken: accessToken });
+  res.json({ message: `Private mode! Hello ${user.name}` });
+});
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
